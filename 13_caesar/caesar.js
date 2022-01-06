c06f0c59e34cda63af24b6609ffffec184379bf5
BearintheWillows <SJWillows@outlook.com>
2022-01-06 12:04:55 +0000

    new

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -25,11 +25,11 @@ const caesar = function(...args) {
             letter = letter + element
         }
     });
-       
+       // A - 65, Z -90
+       //a = 97, z = 122 m
 
     return letter
 };
-
 console.log(caesar('W', 5))
 // Do not edit below this line
 module.exports = caesar;
