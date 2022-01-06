01ec865232bc4b9b8f604ec41f51c0b10f8f1f31
BearInTheWillows <SJWillows@outlook.com>
2022-01-06 11:59:11 +0000

    Working on 13_caesar. 2 test current passing.

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,35 @@
-const caesar = function() {
+const caesar = function(...args) {
+    let letter = ''
+    let array = []
+    let step = 0
+    console.log(args)
 
+    args.forEach(element => {
+        console.log(element)
+        if (typeof element === 'number') {
+            step = element
+        } else {
+            
+            for (let index = 0; index < element.length; index++) {
+                const element2 = element[index];
+                array.push(element2)
+            }
+        
+    }})
+    console.log(array)
+    array.forEach(element => {
+        if (element.match(/[A-Z|a-z]/i)) {
+        let uniCode = element.charCodeAt(element.index) + step
+        letter = letter + String.fromCharCode(uniCode)
+        } else {
+            letter = letter + element
+        }
+    });
+       
+
+    return letter
 };
 
+console.log(caesar('W', 5))
 // Do not edit below this line
 module.exports = caesar;
