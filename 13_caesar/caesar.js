9e892fcfc6094aebd107056e4d93b2705727f7bb
BearintheWillows <SJWillows91@outlook.com>
2022-01-08 22:04:47 +0000

    Completed 13_caesar
    Passed all tests for caesar.js

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,37 +1,71 @@
-const caesar = function(...args) {
+function caesar(...args) {
+    let unicodeString = []
+    let capitalArray = []
+    let lowerArray = []
+    let step
     let letter = ''
-    let array = []
-    let step = 0
-    console.log(args)
 
+    const formCapital = () => {
+        let num = 65
+        for (let index = 0; index < 26; index++) {
+        capitalArray.push(num);
+        num++
+         }}
+
+    const formLower = () => {
+        let num = 97
+        for (let index = 0; index < 26; index++) {
+            lowerArray.push(num)
+            num++
+        }
+    }
+    
     args.forEach(element => {
-        console.log(element)
         if (typeof element === 'number') {
             step = element
         } else {
-            
             for (let index = 0; index < element.length; index++) {
-                const element2 = element[index];
-                array.push(element2)
-            }
-        
-    }})
-    console.log(array)
-    array.forEach(element => {
-        if (element.match(/[A-Z|a-z]/i)) {
-        let uniCode = element.charCodeAt(element.index) + step
-        letter = letter + String.fromCharCode(uniCode)
-        } else {
-            letter = letter + element
+                unicodeString.push(element[index])
+            }}
+        })
+
+        formCapital()
+        formLower()
+
+        let mod = (x, m) => {
+            return (x % m + m) % m;
         }
-    });
-       // A - 65, Z -90
+        
+        unicodeString.forEach(element => {
+            step = step % 26
+            let count = 0
+            if (element.match(/[A-Z|a-z]/i)) {
+            let uniCode = element.charCodeAt(element.index)
+                if (uniCode >= 65 && uniCode <= 90) {
+                   
+                count = capitalArray.indexOf(uniCode)
+                count = mod((count + step), capitalArray.length)
+                uniCode = capitalArray[count]
+                } else if (uniCode >= 97 && uniCode <= 122) {
+                    count = lowerArray.indexOf(uniCode)
+                    count = mod((count + step), lowerArray.length)
+                    uniCode = lowerArray[count]
+            
+                }
+            letter = letter + String.fromCharCode(uniCode)
+            } else {
+                letter = letter + element
+            }
+        });
+        return letter
+    }
+
+    console.log(caesar('Mjqqt, Btwqi!', -5))
+
+// A - 65, Z -90
        //a = 97, z = 122 
 
-       //new commit
 
-    return letter
-};
-console.log(caesar('W', 5))
+
 // Do not edit below this line
 module.exports = caesar;
